d09785702f5db264280855a8f684634683097dbc
Gergő Gálig <gergogalig@gergos-mbp.t.hu>
2024-03-25 10:07:24 +0100

    Map get tiles based on position

--- WarehouseSimulator/Assets/Scripts/Model/Map.cs
+++ WarehouseSimulator/Assets/Scripts/Model/Map.cs
@@ -10,6 +10,21 @@ public class Map
     
     public Vector2Int MapSize => mapSize;
     
+    public TileType GetTileAt(Vector2Int position)
+    {
+        if (position.x < 0 || position.x >= mapSize.x || position.y < 0 || position.y >= mapSize.y)
+            return TileType.Wall; //implied value outsize the map
+        return mapRepresentaion[position.y * mapSize.x + position.x];
+    }
+    
+    public TileType GetTileAt(int i)
+    {
+        if(i < 0 || i >= mapRepresentaion.Count)
+            return TileType.Wall; //implied value outsize the map
+        return mapRepresentaion[i];
+    }
+    
+    
     public Map()
     {
         mapRepresentaion = new List<TileType>();
