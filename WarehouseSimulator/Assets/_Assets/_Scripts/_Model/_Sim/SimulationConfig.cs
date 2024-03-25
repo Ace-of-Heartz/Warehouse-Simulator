f1315a47eb10444a573b939cf2c6d88ef4c2ff54
Gergő Gálig <gergogalig@Gergos-MacBook-Pro.local>
2024-03-25 16:16:27 +0100

    Created simulation config

---
+++ WarehouseSimulator/Assets/_Assets/_Scripts/_Model/_Sim/SimulationConfig.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace WarehouseSimulator.Model.Sim
+{
+    [Serializable]
+    public struct SimulationConfig
+    {
+        [NonSerialized] public string basePath;// folder which contains the config file
+        
+        public string mapFile;
+        public string agentFile;
+        public int teamSize;
+        public string taskFile;
+        public int numTasksReveal;
+        public string taskAssignmentStrategy;
+    }
+}
