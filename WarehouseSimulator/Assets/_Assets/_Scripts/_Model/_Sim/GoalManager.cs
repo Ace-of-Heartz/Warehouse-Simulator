e1cb4bc705acc1a17696e00f3b99177e7f14dd02
Blaaa Andras <dj3c8j@inf.elte.hu>
2024-03-27 18:00:09 +0100

    Added reader function to GoalManager class

--- WarehouseSimulator/Assets/_Assets/_Scripts/_Model/_Sim/GoalManager.cs
+++ WarehouseSimulator/Assets/_Assets/_Scripts/_Model/_Sim/GoalManager.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using UnityEngine;
 
@@ -8,9 +9,9 @@ namespace WarehouseSimulator.Model.Sim
     {
         private List<Goal> _goalsRemaining; //kérdés: ha ezek csak a még ki nem osztott célok, akkor már aktív célokat mi fogja tartalmazni?
 
-        public GoalManager(List<Goal> gs)
+        public GoalManager()
         {
-            _goalsRemaining = gs;
+            _goalsRemaining = new List<Goal>();
         }
 
         public void AddNewGoal(Vector2Int hir)
@@ -23,6 +24,36 @@ namespace WarehouseSimulator.Model.Sim
             var next = _goalsRemaining.First();
             _goalsRemaining.Remove(next);
             return next;
+        }
+
+        public void ReadGoals(string from, Vector2Int mapSize)
+        {
+            using StreamReader riiid = new(from);
+            if (!int.TryParse(riiid.ReadLine(),out int goaln)) 
+            {throw new InvalidDataException("Invalid file format: First line not a number"); }
+
+            int runningGoaln = 0;
+            string line = riiid.ReadLine();
+            while (line != null)
+            {
+                if (runningGoaln >= goaln)
+                {
+                    throw new InvalidDataException("Invalid file format: there were too many lines");
+                }
+
+                if (!int.TryParse(riiid.ReadLine(), out int linPos)) 
+                {
+                    throw new InvalidDataException($"Invalid file format: {runningGoaln + 2}. line not a number");
+                }
+
+                int quot = linPos / mapSize.y;
+                Vector2Int newGoalPos = new(linPos - mapSize.y * quot,quot);
+                _goalsRemaining.Add(new Goal(newGoalPos));
+
+                runningGoaln++;
+                line = riiid.ReadLine();
+            }
+
         }
     }
 }
