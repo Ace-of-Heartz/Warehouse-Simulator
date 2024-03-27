8dbc14415cf985c0950c8a46a6bfeacb817827bc
Blaaa Andras <dj3c8j@inf.elte.hu>
2024-03-27 17:46:31 +0100

    GoalManager class completed

---
+++ WarehouseSimulator/Assets/_Assets/_Scripts/_Model/_Sim/GoalManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace WarehouseSimulator.Model.Sim
+{
+    public class GoalManager
+    {
+        private List<Goal> _goalsRemaining; //kérdés: ha ezek csak a még ki nem osztott célok, akkor már aktív célokat mi fogja tartalmazni?
+
+        public GoalManager(List<Goal> gs)
+        {
+            _goalsRemaining = gs;
+        }
+
+        public void AddNewGoal(Vector2Int hir)
+        {
+            _goalsRemaining.Add(new Goal(hir));
+        }
+
+        public Goal GetNext()
+        {
+            var next = _goalsRemaining.First();
+            _goalsRemaining.Remove(next);
+            return next;
+        }
+    }
+}
