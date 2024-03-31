1de636937e76ece097ab8e999ba2212c2432e996
Gergő Gálig <gergogalig@gergos-mbp.t.hu>
2024-03-31 20:27:37 +0200

    Added debug mode

--- WarehouseSimulator/Assets/_Assets/_Scripts/_View/_Sim/UnitySimulationManager.cs
+++ WarehouseSimulator/Assets/_Assets/_Scripts/_View/_Sim/UnitySimulationManager.cs
@@ -10,19 +10,33 @@ namespace WarehouseSimulator.View.Sim
         private UnityMap unityMap;
     
         private SimulationManager simulationManager;
+
+        public bool DebugMode = false;
+        public SimInputArgs debugSimInputArgs = new SimInputArgs();
         
         void Start()
         {
             simulationManager = new SimulationManager();
-            simulationManager.Setup(MainMenuManager.simInputArgs);
+            if (DebugMode)
+            {
+                DebugSetup();
+                simulationManager.Setup(debugSimInputArgs);
+            }
+            else
+                simulationManager.Setup(MainMenuManager.simInputArgs);
 
             unityMap.AssignMap(simulationManager.Map);
             unityMap.GenerateMap();
         }
 
-        void Update()
+        void DebugSetup()
         {
-        
+            debugSimInputArgs.ConfigFilePath = "/Users/gergogalig/Library/CloudStorage/OneDrive-EotvosLorandTudomanyegyetem/FourthSemester/Szofttech/sample_files/warehouse_100_config.json";
+            debugSimInputArgs.PreparationTime = 1;
+            debugSimInputArgs.IntervalOfSteps = 1;
+            debugSimInputArgs.NumberOfSteps = 100;
+            debugSimInputArgs.EventLogPath = "/Users/gergogalig/log.log";
+            
         }
     }   
 }
