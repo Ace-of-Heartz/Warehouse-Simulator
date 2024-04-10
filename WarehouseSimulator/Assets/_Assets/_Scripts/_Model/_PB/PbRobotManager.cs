33187ea1d71c51f6fd67f5b719e3dc8ac06d1395
Blaaa Andras <dj3c8j@inf.elte.hu>
2024-04-10 22:06:57 +0200

    PbRobot and PbRobotManager classes ready for basic usage

---
+++ WarehouseSimulator/Assets/_Assets/_Scripts/_Model/_PB/PbRobotManager.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using WarehouseSimulator.Model.Enums;
+
+namespace WarehouseSimulator.Model.PB
+{
+    public class PbRobotManager
+    {
+        private List<PbRobot> _allRobots;
+
+        public PbRobotManager()
+        {
+            _allRobots = new();
+        }
+
+        public void SetUpAllRobots(int stepNumber,List<(Vector2Int,Direction)> whoWhere,List<List<RobotDoing>> robiesDoing)
+        {
+            if (stepNumber < robiesDoing.Count)
+            {
+                throw new ArgumentException($"Argument {nameof(stepNumber)} with value {stepNumber} too low");
+            }
+
+            if (stepNumber > robiesDoing.Count)
+            {
+                throw new ArgumentException($"Argument {nameof(stepNumber)} with value {stepNumber} too high");
+            }
+            
+            int i = 1;
+            foreach ((var coordin, var dirr) in whoWhere)
+            {
+                var robie = new PbRobot(i, coordin, stepNumber,dirr);
+                robie.CalcTimeLine(robiesDoing[i-1]); //TODO => Blaaa: async?
+                i++;
+                _allRobots.Add(robie);
+            }
+        }
+
+        public void SetTimeTo(int step)
+        {
+            foreach (PbRobot robie in _allRobots)
+            {
+                robie.SetTimeTo(step); //TODO => Blaaa: async?
+            }
+        }
+    }
+}
