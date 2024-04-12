52079587e99535575dacb21380a78b979ad6ddfe
Blaaa Andras <dj3c8j@inf.elte.hu>
2024-04-12 10:25:51 +0200

    Structs created for Log

---
+++ WarehouseSimulator/Assets/_Assets/_Scripts/_Model/_Structs/TaskEventInfo.cs
@@ -0,0 +1,16 @@
+﻿namespace WarehouseSimulator.Model.Structs
+{
+    public struct TaskEventInfo
+    {
+        public int Task { private set; get; }
+        public int Step { private set; get; }
+        public string WhatHappened { private set; get; }
+
+        public TaskEventInfo(int task, int step, string whatHappened)
+        {
+            this.Task = task;
+            this.Step = step;
+            this.WhatHappened = whatHappened;
+        }
+    }
+}
