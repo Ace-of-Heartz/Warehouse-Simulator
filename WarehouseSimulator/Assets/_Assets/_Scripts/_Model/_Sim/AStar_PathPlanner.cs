71d4af51f19e10ea0ebef11f3f48b016313ce0a5
Ferenci Ákos <aqij3g@inf.elte.hu>
2024-04-13 16:45:20 +0200

    Added A star path planner class

---
+++ WarehouseSimulator/Assets/_Assets/_Scripts/_Model/_Sim/AStar_PathPlanner.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using UnityEngine;
+using WarehouseSimulator.Model.Enums;
+
+namespace WarehouseSimulator.Model.Sim
+{
+    public class AStar_PathPlanner : IPathPlanner
+    {
+        public async Task<List<RobotDoing>> GetPath(Vector2Int start, Vector2Int finish)
+        {
+            List<RobotDoing> instructions = new();
+
+            return instructions;
+            
+        }
+    }
+}
