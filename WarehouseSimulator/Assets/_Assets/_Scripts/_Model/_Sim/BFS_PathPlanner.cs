386ce0f131e775e9b896c696bd9892ab3f197419
Ferenci Ákos <aqij3g@inf.elte.hu>
2024-04-13 16:44:49 +0200

    Added BFS path planner

---
+++ WarehouseSimulator/Assets/_Assets/_Scripts/_Model/_Sim/BFS_PathPlanner.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using UnityEngine;
+using WarehouseSimulator.Model.Enums;
+using WarehouseSimulator.Model.Sim;
+
+namespace WarehouseSimulator.Model.Sim
+{
+    public class BFS_PathPlanner : IPathPlanner
+    {
+        public async Task<List<RobotDoing>> GetPath(Vector2Int start, Vector2Int finish)
+        {
+            List<RobotDoing> instructions = new();
+
+            return instructions;
+        }
+    }
+}
