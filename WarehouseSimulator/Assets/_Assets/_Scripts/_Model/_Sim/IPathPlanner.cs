a0743f1a1a71d6ed9fac74929547fae33c103dda
Ferenci Ákos <aqij3g@inf.elte.hu>
2024-04-13 16:44:28 +0200

    Added interface for path planner classes

---
+++ WarehouseSimulator/Assets/_Assets/_Scripts/_Model/_Sim/IPathPlanner.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using UnityEngine;
+using WarehouseSimulator.Model.Enums;
+using Vector2 = System.Numerics.Vector2;
+
+namespace WarehouseSimulator.Model.Sim
+{
+    public interface IPathPlanner
+    {
+        // ReSharper restore Unity.ExpensiveCode
+        public Task<List<RobotDoing>> GetPath(Vector2Int start, Vector2Int finish);
+        
+        
+    }
+}
