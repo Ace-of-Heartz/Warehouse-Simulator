9fee807f9507b50bf02e69c50ee7cb0b4a98cc42
Blaaa Andras <dj3c8j@inf.elte.hu>
2024-04-14 07:38:37 +0200

    RobotLike class created to be the future base class of the robots of the model

---
+++ WarehouseSimulator/Assets/_Assets/_Scripts/_Model/RobotLike.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using WarehouseSimulator.Model.Enums;
+
+namespace WarehouseSimulator.Model
+{
+    public abstract class RobotLike
+    {
+        #region Fields
+        private readonly int _id;
+        private Vector2Int _currentGridPosition;
+        private Direction _currentHeading;
+        private RobotBeing _currentState;
+        #endregion
+        
+        #region Properties
+        public int Id
+        {
+            get => _id;
+        }
+        public Vector2Int GridPosition
+        {
+            get => _currentGridPosition;
+        }
+        public Direction Heading
+        {
+            get => _currentHeading;
+        }
+        
+        public RobotBeing State
+        {
+            get => _currentState;
+        }
+        #endregion
+        
+        protected RobotLike(int i, Vector2Int gPos, Direction h = Direction.North, RobotBeing s = RobotBeing.Free)
+        {
+            _id = i;
+            _currentGridPosition = gPos;
+            _currentHeading = h;
+            _currentState = s;
+        }
+        
+        private Vector2Int WhereToMove(Vector2Int pos)
+        {
+            switch (_currentHeading)
+            {
+                case(Direction.North):
+                    return pos + Vector2Int.down; 
+                case(Direction.West):
+                    return pos + Vector2Int.left; 
+                case(Direction.South):
+                    return pos + Vector2Int.up;
+                case(Direction.East):
+                    return pos + Vector2Int.right;
+                default: return new Vector2Int(0, 0);
+            }
+        }
+    }
+}
