2fe4e5c2e305c3d94aab3b6e96c59b810b22d6de
Ferenci Ákos <aqij3g@inf.elte.hu>
2024-04-20 23:46:40 +0200

    Added Heap implementation

---
+++ WarehouseSimulator/Assets/_Assets/_Scripts/_Model/MinHeap.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+
+
+namespace WarehouseSimulator.Model
+{
+    public class MinHeap<T,S> : Heap<T,S>
+    {
+        public MinHeap()
+            : this(Comparer<T>.Default)
+        {
+        }
+
+        public MinHeap(Comparer<T> comparer)
+            : base(comparer)
+        {
+        }
+
+        public MinHeap(IEnumerable<Tuple<T,S>> collection) : base(collection)
+        {
+        }
+
+        public MinHeap(IEnumerable<Tuple<T,S>> collection, Comparer<T> comparer)
+            : base(collection, comparer)
+        {
+        }
+
+        protected override bool Dominates(Tuple<T,S> x, Tuple<T,S> y)
+        {
+            return Comparer.Compare(x.Item1, y.Item1) <= 0;
+        }
+    }
+}
