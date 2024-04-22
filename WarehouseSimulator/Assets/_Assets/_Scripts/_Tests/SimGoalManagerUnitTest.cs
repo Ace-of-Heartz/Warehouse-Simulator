a3b0527f38ae69d2e1f5b9a1e3e3fd1b80c3435d
Blaaa Andras <dj3c8j@inf.elte.hu>
2024-04-22 14:26:42 +0200

    SimulationManager minor change, SimGoalManagerUnitTests adjusted to match Goal manager function

--- WarehouseSimulator/Assets/_Assets/_Scripts/_Tests/SimGoalManagerUnitTest.cs
+++ WarehouseSimulator/Assets/_Assets/_Scripts/_Tests/SimGoalManagerUnitTest.cs
@@ -1,5 +1,7 @@
-﻿using NUnit.Framework;
+﻿using System;
+using NUnit.Framework;
 using UnityEngine;
+using WarehouseSimulator.Model;
 using WarehouseSimulator.Model.Sim;
 
 namespace _Assets._Scripts._Tests
@@ -8,11 +10,15 @@ namespace _Assets._Scripts._Tests
     public class SimGoalManagerUnitTest
     {
         private SimGoalManager _golieMan;
+        private Map _33Map;
 
         [SetUp]
         public void Setup()
         {
             _golieMan = new();
+            string[] input = {"h 3","w 3","map","...","...","..."};
+            _33Map = new();
+            _33Map.CreateMap(input);
         }
 
         [TestCase(1)]
@@ -23,7 +29,7 @@ namespace _Assets._Scripts._Tests
             
             for (int i = 0; i < addedGoalsNumber; ++i)
             {
-                _golieMan.AddNewGoal(Vector2Int.one);
+                _golieMan.AddNewGoal(Vector2Int.one,_33Map);
             }
 
             int nIncrease = _golieMan.GoalCount - oldN;
@@ -31,10 +37,19 @@ namespace _Assets._Scripts._Tests
             Assert.AreEqual(addedGoalsNumber,nIncrease);
         }
 
+        [TestCase(-1, -1)]
+        [TestCase(2, 2)]
+        public void AddNewGoal_ResultingExceptionThrown(int x, int y)
+        {
+            string[] input = {"h 3","w 3","map","...",".@.","..."};
+            _33Map = new();
+            Assert.Throws<ArgumentException>(() => _golieMan.AddNewGoal(new Vector2Int(x, y), _33Map));
+        }
+
         [Test]
         public void GetNext_ResultingNonNullReturn()
         {
-            _golieMan.AddNewGoal(Vector2Int.one);
+            _golieMan.AddNewGoal(Vector2Int.one,_33Map);
             Assert.AreNotEqual(null,_golieMan.GetNext());
         }
         
