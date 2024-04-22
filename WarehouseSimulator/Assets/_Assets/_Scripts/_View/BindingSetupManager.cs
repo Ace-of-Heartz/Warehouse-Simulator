12ea55c4e1f4d90f89adcd310acb82ca84a3e6c7
Ferenci Ákos Sándor <aqij3g@inf.elte.hu>
2024-04-22 11:12:36 +0200

    Added MessageBox for input errors for goal addition

--- WarehouseSimulator/Assets/_Assets/_Scripts/_View/BindingSetupManager.cs
+++ WarehouseSimulator/Assets/_Assets/_Scripts/_View/BindingSetupManager.cs
@@ -95,7 +95,18 @@ namespace WarehouseSimulator.View
 
             button.clickable.clicked += () =>
             {
-                man.SimGoalManager.AddNewGoal(coordinatesField.value,man.Map);
+                try
+                {
+                    man.SimGoalManager.AddNewGoal(coordinatesField.value, man.Map);
+                }
+                catch (ArgumentException e)
+                {
+                    UIMessageManager.GetInstance().MessageBox(e.Message,
+                        response => { },
+                        new OneWayMessageBoxTypeSelector(OneWayMessageBoxTypeSelector.MessageBoxType.OK));
+                }
+                
+                 
             };
 
         }
